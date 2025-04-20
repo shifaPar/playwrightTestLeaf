6d6be93f6bc23f6ef8c79613aedc8847b1d55353
Shifa Parveen <shifaparveen11@gmail.com>
2025-04-20 18:46:37 +0530

    Add files via upload
    week1.day1 - Home Assignement - Primitive data types

---
+++ premitiveDataTypes.js
@@ -0,0 +1,19 @@
+let firstName = "Shifa"
+console.log(firstName)
+console.log(typeof(firstName))
+
+let companyName = 'Acqueon'
+console.log(companyName)
+console.log(typeof(companyName))
+
+let mobileNumber = 9089786756
+console.log(mobileNumber)
+console.log(typeof(mobileNumber))
+
+let isAutomation = true
+console.log(isAutomation)
+console.log(typeof(isAutomation))
+
+let hasPlaywright
+console.log(hasPlaywright)
+console.log(typeof(hasPlaywright))
