16e21202e6f379d58f90bdd475685de705068394
Shifa Parveen <shifaparveen11@gmail.com>
2025-04-20 19:07:03 +0530

    Add files via upload
    week1.day1 - Home Assignments - Number type

---
+++ numberType.js
@@ -0,0 +1,14 @@
+let num = 10
+function numtype (num){
+    if(num>0){
+        console.log("Given number is positive")
+    }
+    else if(num==0){
+        console.log("Given number is 0")
+    }
+    else{
+        console.log("Given number is negative")
+    }
+}
+
+numtype(0)
