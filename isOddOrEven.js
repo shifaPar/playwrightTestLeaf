a3c7c59c3455ee125dfab0db3f03fa29d9bcccf3
Shifa Parveen <shifaparveen11@gmail.com>
2025-04-20 18:42:06 +0530

    Add files via upload

---
+++ isOddOrEven.js
@@ -0,0 +1,10 @@
+isOddOrEven(15)
+
+function isOddOrEven(number){
+        if(number%2 == 0){
+            console.log("Given number is Even")
+        }
+        else{
+            console.log("Given number is odd")
+        }
+}
